b516f885fd3e28d1bf467b277bbab33c529062ba
Paola Andrea Chavez Vallejos <e98474u@etu.univ-lorraine.fr>
2025-09-18 11:26:05 +0200

    Modified CMake, HelloProjConfig and hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include<HelloProjConfig.h>
+
 void hello_world() {
 	std::cout << "Hello World!\n";
+}
+void print_version() {
+	std::cout << "project version is "
+		<< HelloProj_VERSION_MAJOR
+		<< "."
+		<< HelloProj_VERSION_MINOR
+		<< std::endl;
 }
 int main()
 {
 	hello_world();
+	print_version();
 }
